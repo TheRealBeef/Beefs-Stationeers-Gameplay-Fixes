6dda1a8a305eb156a05e2901c64011324a785d70
TheRealBeef <michael.d.friske@outlook.com>
2021-06-05 17:09:46 +0200

    Add hunger/hydration changes

--- patches.cs
+++ patches.cs
@@ -2,15 +2,11 @@
 using HarmonyLib;
 using UnityEngine;
 using Assets.Scripts.Vehicles;
+using Assets.Scripts.Objects;
 using UnityStandardAssets.ImageEffects;
 
 namespace Beefy_Fixes
 {
-    public class PatchConfigs
-    {
-        public static bool I_Have_A_Strong_PC_And_GPU = true; // Determines whether to use quarter or half resolution, and how many samples.
-    }
-
     // Changes to Rover
     [HarmonyPatch(typeof(Rover), "Awake")]
     public class RoverPatcher
@@ -20,8 +16,23 @@ namespace Beefy_Fixes
             Rover RoverPatcher = __instance as Rover;
 
             RoverPatcher.MaxSpeed = 24.0f; //3.0f original
-            RoverPatcher.MotorPower = 40.0f; //20.0f original
-            RoverPatcher.BrakePower = 15.0f; //5.0f original
+            RoverPatcher.MotorPower = 30.0f; //20.0f original
+            RoverPatcher.BrakePower = 10.0f; //5.0f original
+        }
+    }
+
+    // Changes to Rover
+    [HarmonyPatch(typeof(Entity), "Awake")]
+    public class HungerHydrationPatcher
+    {
+        static void Postfix(Entity __instance)
+        {
+            Entity HungerHydrationPatcher = __instance as Entity;
+            HungerHydrationPatcher.MaxNutritionStorage = 30.0f; //5.0f original
+        }
+        static void Postfix()
+        {
+           Entity.MaxHydrationStorage = 20.0f; //5.0f original
         }
     }
 }
