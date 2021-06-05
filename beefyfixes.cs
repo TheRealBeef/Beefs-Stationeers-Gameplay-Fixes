7913b33c61ffe22b1044f0951b59419e78274659
TheRealBeef <michael.d.friske@outlook.com>
2021-06-05 15:59:51 +0200

    Add project files.

---
+++ beefyfixes.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using HarmonyLib;
+using BepInEx;
+using UnityEngine;
+
+namespace BeefyFixes
+{
+    [BepInPlugin("org.bepinex.plugins.beeffixes", "Beef's Gameplay Fixes", "0.1")]
+    [BepInProcess("rocketstation.exe")]
+    public class init : BaseUnityPlugin
+    {
+        void Awake()
+        {
+            beefyfixes.Awake();
+        }
+    }
+
+    public class beefyfixes
+    {
+        // Variable Definitions
+
+        public static void AppendLog(string logdetails)
+        {
+            UnityEngine.Debug.Log("Beef's Gameplay Fixes - " + logdetails);
+        }
+
+        // Awake is called once when both the game and the plug-in are loaded
+        public static void Awake()
+        {
+            //Initialize();
+
+            AppendLog("Initialized");
+            var harmony = new Harmony("org.bepinex.plugins.beeffixes");
+            harmony.PatchAll();
+            AppendLog("Patched with Harmony");
+        }
+    }
+}
---
+++ patches.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using HarmonyLib;
+using UnityEngine;
+using Assets.Scripts.Vehicles;
+using UnityStandardAssets.ImageEffects;
+
+namespace Beefy_Fixes
+{
+    public class PatchConfigs
+    {
+        public static bool I_Have_A_Strong_PC_And_GPU = true; // Determines whether to use quarter or half resolution, and how many samples.
+    }
+
+    // Changes to Rover
+    [HarmonyPatch(typeof(Rover), "Awake")]
+    public class RoverPatcher
+    {
+        static void Postfix(Rover __instance)
+        {
+            Rover RoverPatcher = __instance as Rover;
+
+            RoverPatcher.MaxSpeed = 24.0f; //3.0f original
+            RoverPatcher.MotorPower = 40.0f; //20.0f original
+            RoverPatcher.BrakePower = 15.0f; //5.0f original
+        }
+    }
+}
